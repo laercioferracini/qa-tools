36353a5ee930086de233639a69df529c67c7b5c5
laercioferracini <laercio_ferracini@yahoo.com.br>
2021-09-13 15:35:38 -0300

    Configuring nodemon. env variables

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const chalk = require('chalk'); //colorized you output
 const debug = require('debug')('qatools') //smart way to debug apps
 const morgan = require('morgan') //depency to check requests on app.
 const path = require('path')
+
+const PORT = process.env.PORT || 3000
 const app = express();
+
 // app.use(morgan('combined'));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'/public/')))
@@ -12,7 +15,7 @@ app.get('/', (req, res) => {
     res.send('Hello from QA Tools')
 })
 
-app.listen(3000, () => { debug(`listening on port ${chalk.green('3000')}`) }) // example of template string `${whatever}`
+app.listen(3000, () => { debug(`listening on port: ${chalk.green(PORT)}`) }) // example of template string `${whatever}`
 
 /**
  * debug
